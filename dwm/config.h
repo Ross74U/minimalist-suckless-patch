3fc0f62002a5c4f6e38933d43eb6c0af8fbb2952
Ross74U <jasonshen0518@gmail.com>
2025-03-14 00:45:00 -0400

    modified fonts

--- dwm/config.h
+++ dwm/config.h
@@ -8,7 +8,7 @@ static const unsigned int gappx	    = 1;	/* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -19,7 +19,7 @@ static const char col_red[]	    = "#dc143c"; // red color for border
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_red  },
 };
 
 /* tagging */
